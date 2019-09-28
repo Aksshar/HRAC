f944a2aa6505d83b41086d4a96b3c11069b18f9c
Aksshar Ramesh <37536002+Aksshar@users.noreply.github.com>
2019-09-28 19:28:34 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
+import { RouterModule,Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SigninComponent } from './signin/signin.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AttendanceComponent } from './attendance/attendance.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SigninComponent,
+    HomeComponent,
+    SignUpComponent,
+    AttendanceComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      {path: '',component: HomeComponent},
+      { path: 'Home', component: HomeComponent },
+      { path: 'SignIn', component: SigninComponent },
+      { path: 'SignUp', component: SignUpComponent },
+      { path: 'Attendance', component: AttendanceComponent}
+    ]),
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private router: RouterModule) { }
+
+  ngOnInit() {
+  }
+
+}
