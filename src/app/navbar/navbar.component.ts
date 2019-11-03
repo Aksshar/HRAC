359640019abc7e8e5d6c6e4dec8d6a9bf1779b41
Aksshar Ramesh <37536002+Aksshar@users.noreply.github.com>
2019-11-03 14:24:18 +0530

    "signup implemented"

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
+import { AngularFireAuth } from '@angular/fire/auth';
 import { RouterModule } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { User } from 'firebase';
 import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'navbar',
@@ -11,7 +13,19 @@ import { Observable } from 'rxjs';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router: RouterModule,public authService: AuthService) { }
+  user: firebase.User;
+  email;
+  constructor(private router: RouterModule, public authService: AuthService, private afAuth: AngularFireAuth) {
+   afAuth.authState.subscribe(res => {
+    if (res && res.uid) {
+      console.log('user is logged in');
+      this.email = res.email;
+    } else {
+      console.log('user not logged in');
+    }
+  });
+   }
+   
 
   ngOnInit() {
   }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormControl } from '@angular/forms';
 import { MustMatch } from './validators';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { map, take, debounceTime} from 'rxjs/Operators';
