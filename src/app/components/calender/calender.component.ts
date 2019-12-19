4bac6ffa001b8f085f9e0b2d6ea032e5c27338c5
Aksshar <rameshakshar700@gmail.com>
2019-12-19 15:35:37 +0530

    calender connected with firebase

--- src/app/components/calender/calender.component.ts
+++ src/app/components/calender/calender.component.ts
@@ -1,22 +1,44 @@
+import { AngularFirestore } from 'angularfire2/firestore';
 import { Component, OnInit } from '@angular/core';
 import { EventSettingsModel, View, DayService, WeekService, WorkWeekService, MonthService, AgendaService } from '@syncfusion/ej2-angular-schedule';
 import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+import { BookingDate } from './booking-data';
+
 
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
   styleUrls: ['./calender.component.css']
 })
+  
+  
 export class CalenderComponent implements OnInit {
+  public dataList: Array<BookingDate> = [];
+  list;
   public selectedDate: Date = new Date();
   public currentView: View = 'Day';
   public readonly: boolean = true;
+  public eventSettings: EventSettingsModel;
   //public eventSettings: EventSettingsModel = { dataSource: this.dataManger };
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
+    
+    this.db.collection('booking').valueChanges().subscribe(ref => {
+      this.list = ref;
+      console.log(this.list);
+      for (let i of this.list) {
+        let temp = new BookingDate();
+        console.log(new Date(i.stime));
+        temp.StartTime = new Date(i.stime);
+        console.log(temp);
+        temp.EndTime = new Date(i.etime);
+        this.dataList.push(temp);
+      }
+      this.eventSettings = { dataSource: this.dataList};
+    });
+    
   }
-
 }
 
 
