6b2d57c02ab0a68cec7ba4ea09e590d7365940ea
Aksshar <rameshakshar700@gmail.com>
2019-12-19 11:31:12 +0530

    some changes on booking ui

--- src/app/hall-reservation/hall-reservation.component.ts
+++ src/app/hall-reservation/hall-reservation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-hall-reservation',
@@ -6,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hall-reservation.component.css']
 })
 export class HallReservationComponent implements OnInit {
+  closeResult: string;
+  constructor(private modalService: NgbModal) { }
 
-  constructor() { }
+  openLg(content) {
+    this.modalService.open(content,{size: 'lg'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
   ngOnInit() {
   }
 
