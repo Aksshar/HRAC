0764e4605f10cabfc25f9318c11dd0903ceec176
sandu <sanduni.madu1996@gmail.com>
2020-01-04 23:56:17 +0530

    edit manage.booking.component.ts

---
+++ src/app/booking.model.ts
@@ -0,0 +1,9 @@
+export class Booking {
+    id:string;
+    name:string;
+    session:string;
+    code:string;
+      lecture:string;
+    //time:string;
+
+}
--- src/app/manage-booking/manage-booking.component.ts
+++ src/app/manage-booking/manage-booking.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { NewBookingService } from '../new-booking.service';
+import { Booking } from '../booking.model';
 
 @Component({
   selector: 'app-manage-booking',
@@ -7,9 +9,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./manage-booking.component.css']
 })
 export class ManageBookingComponent implements OnInit{
+  list:Booking[];
+  constructor(private service:NewBookingService){}
 
   ngOnInit() {
-   
+   this.service.getBooking().subscribe(res=>{
+     this.list= res.map(item =>{
+       return{
+         id: item.payload.doc.id,
+         //...item.payload.doc.data()
+        } as Booking
+     })
+   });
     
   }
 
