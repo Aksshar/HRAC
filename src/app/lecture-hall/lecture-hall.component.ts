9f75ed29627908a8ff93ff21902f28ff539d9378
Aksshar <rameshakshar700@gmail.com>
2020-01-08 19:18:46 +0530

    new component lecturehall created

---
+++ src/app/lecture-hall/lecture-hall.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lecture-hall',
+  templateUrl: './lecture-hall.component.html',
+  styleUrls: ['./lecture-hall.component.css']
+})
+export class LectureHallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
