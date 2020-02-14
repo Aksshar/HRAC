11c3e510ec7911cc003f6e00237b87825b0b47fe
sanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-02-14 15:40:18 +0530

    Add funtion to retrive data

--- src/app/manage-booking/manage-booking.component.ts
+++ src/app/manage-booking/manage-booking.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFirestore} from 'angularfire2/firestore';
+import { ManagebookingService } from '../managebooking.service';
+
+
 //import 'rxjs/add/operator/toPromise';
 
 @Component({
@@ -7,19 +11,52 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './manage-booking.component.html',
   styleUrls: ['./manage-booking.component.css']
 })
+
+
 export class ManageBookingComponent implements OnInit{
 
+
+  constructor(private service: ManagebookingService,private afs: AngularFirestore) { }
+ 
   ngOnInit() {
-   
-    
-  
-  /*
-  this.firestore.collection("new_booking_requests").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-    });
-});
-*/
+    /*this.service.getEmployees().subscribe(actionArray => {
+      this.list = actionArray.map(item => {
+        return {
+          id: item.payload.doc.id,
+          ...item.payload.doc.data() 
+        } as request;
+      })
+    });*/
+    this.getUsers1();
+    this.getUsers2();
+    this.getUsers3();
+  }
+  list1;
+  list2;
+  list3;
+
+  getUsers1 = () =>
+  this.service.getRequest()
+    .subscribe(res => (this.list1 = res)
+    );
+  getUsers2 = () =>
+  this.service.getConfirm()
+    .subscribe(res => (this.list2 = res)
+    );
+  getUsers3 = () =>
+  this.service.getReject()
+    .subscribe(res => (this.list3 = res)
+    );
+
+confirm(){
+  let data = this.afs.collection("new_booking_requests").doc(order.payload.doc.id);
+  insertIndex(data);
+  return this.afs.collection("new_booking_requests").doc(data.payload.doc.id).delete();
 }
+
+reject(){
+  
+  return this.afs.collection("new_booking_requests").doc(data.payload.doc.id).delete();
+}
+
 }
